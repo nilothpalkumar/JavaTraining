a19ed393a54ccfd559a937e9c2d50110c42b4cae
Nilothpal Kumar <Nilothpal.Kumar@godigit.com>
2024-07-25 11:41:04 +0530

    Operator programs Assignment problems

---
+++ AssignmentOperatporsQ1.java
@@ -0,0 +1,21 @@
+public class AssignmentOperatporsQ1 {
+    public static void main(String[] args) {
+
+        int a=10;
+
+        a+=2;
+        System.out.println(a);
+
+        a-=2;
+        System.out.println(a);
+
+        a*=2;
+        System.out.println(a);
+
+        a/=2;
+        System.out.println(a);
+
+        a%=2;
+        System.out.println(a);
+    }
+}
---
+++ TernaryOperatorQ1.java
@@ -0,0 +1,9 @@
+public class TernaryOperatorQ1 {
+    public static void main(String[] args) {
+
+         int firstNum = 5;
+         int secondNum = 7;
+         int ans = (firstNum<secondNum) ? firstNum : secondNum;
+         System.out.println("Minimun Number =  "+ ans);
+    }
+}
